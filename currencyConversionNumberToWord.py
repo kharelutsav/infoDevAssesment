63eb6a4d644d9f01774143eceeff72de5a90da70
kharelutsav <69389889+kharelutsav@users.noreply.github.com>
2022-01-02 11:30:09 +0545

    Update currencyConversionNumberToWord.py

--- currencyConversionNumberToWord.py
+++ currencyConversionNumberToWord.py
@@ -1,47 +1,100 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+# Author: KharelUtsav
 
-# Online Python - IDE, Editor, Compiler, Interpreter
-#1234
-num = 1234567000
-# if num == 0:
-#     return 'Zero';
 
-numStr = str(num)
-if 3-(len(numStr) % 3) != 3:  
-    numStr = ((3-(len(numStr) % 3)) * '0') + numStr
-    
+#Arrays containing word representation of necessary numbers.
 high = ['', 'Thousand ', 'Million ', 'Billion ', 'Trillion ']
-Tens = ['', 'Ten ', 'Twenty ', 'Thirty ', 'Forty ', 'Fifty ', 'Sixty ', 'Seventy ', 'Eighty ', 'Ninety ']
-Teens = ['', 'Eleven ', 'Twelve ', 'Thirteen ', 'Fourteen ', 'Fifteen ', 'Sixteen ', 'Seventeen ', 'Eighteen ', 'Nineteen ']
-BelowTen = ['', 'One ', 'Two ', 'Three ', 'Four ', 'Five ', 'Six ', 'Seven ', 'Eight ', 'Nine ']
-length = len(numStr) // 3
-converted = ""
-count = 0
-for i in range(length):
-    one = int(numStr[count + 0])
-    two = int(numStr[count + 1])
-    three = int(numStr[count + 2])
+
+#Numbers containing tens excluding hundred.
+tens = ['', 'Ten ', 'Twenty ', 'Thirty ', 'Forty ', 'Fifty ', 'Sixty ', 'Seventy ', 'Eighty ', 'Ninety ']
+
+#Numbers between ten and twenty
+teens = ['', 'Eleven ', 'Twelve ', 'Thirteen ', 'Fourteen ', 'Fifteen ', 'Sixteen ', 'Seventeen ', 'Eighteen ', 'Nineteen ']
+
+#Numbers below ten
+below_ten = ['', 'One ', 'Two ', 'Three ', 'Four ', 'Five ', 'Six ', 'Seven ', 'Eight ', 'Nine ']
+
+def integerToEnglishCurrency(num): #Argument type: non-negative Number/Integer, Return type: String
+    """
+    integerToEnglishCurrency function takes num parameter of type integer. It checks if number is 0 and returns Zero if true.
+
+    Number is converted string and necessary number of 0's are added to convert strings length into multiple of three.
+
+    Loop through modified String in length of three. Increment count values by 3 and assign converted string to converted variable.
+
+    Return string representation of given currency.
+
+    """
+
+    #If num is 0: return Zero
+    if num == 0:
+        return 'Zero'
+        #Return to caller
+
+    #Else: number to string
+    numStr = str(num)
+
+    #If: String.length % 3 not equal to 0 
+    if 3-(len(numStr) % 3) != 3:  
+        #String => (('0' * n) + String) % 3 equals 0
+        numStr = ((3-(len(numStr) % 3)) * '0') + numStr
     
-    #hundred
-    converted += BelowTen[one]
-    if one > 0:
-        converted += 'Hundred '
+    #Divide length of String by three for looping.
+    length = len(numStr) // 3
     
-    #tens
-    if two == 0:
-        converted += BelowTen[three]
-        
-    elif three == 0:
-        converted += Tens[two]
-        
-    elif two == 1:
-        converted += Teens[three]
+    #Container to store converted string
+    converted = ""
+
+    #Count
+    count = 0
     
-    else:
-        converted += Tens[two] + BelowTen[three]
+    #Iterate through range of length (i.e. 0, 1, ..., length-1)
+    for i in range(length):
+        #Number in hundreds position
+        one = int(numStr[count + 0])
+        #Number in tens position
+        two = int(numStr[count + 1])
+        #Number in one position
+        three = int(numStr[count + 2])
+
+        if one == 0 and two == 0 and three == 0:
+            count += 3
+            continue
         
-    converted += high[length-i-1]
-    count += 3
-print(converted)
+        #Hundred converstion
+        converted += below_ten[one]
+
+        #Concats string Hundred to converted variable only if hundreds position is not equal to zero.
+        if one > 0:
+            converted += 'Hundred '
         
+        #Else if tens position is 1, then just concat string from teens array with converted (i.e '', 'Eleven', ..., 'Nineteen'). 
+        if two == 1:
+            converted += teens[three]
         
+        #Else if tens position is not 1, then concat string from tens array
+        # and below_ten array(i.e '', 'One', ..., 'Nine') with converted (i.e '', 'Eleven', ..., 'Nineteen'). 
+        else:
+            converted += tens[two] + below_ten[three]
+
+        #High values(i.e '', 'Thousand', ..., 'Trillion')    
+        converted += high[length-i-1]
+
+        #Increment count by three
+        count += 3
+        
+    return print(converted)
+
+#Test case: 1
+num1 = 123
+integerToEnglishCurrency(num1)
+
+#Test case: 2
+num2 = 12345
+integerToEnglishCurrency(num2)
+
+#Test case: 3
+num3 = 1234567
+integerToEnglishCurrency(num3)
 
